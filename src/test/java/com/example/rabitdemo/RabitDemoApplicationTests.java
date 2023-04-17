d630b09707868b6c7d7c902ba91805fed9363be8
darkcovboy <47691903+darkcovboy@users.noreply.github.com>
2023-04-17 09:42:46 +0300

    first

---
+++ src/test/java/com/example/rabitdemo/RabitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rabitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
